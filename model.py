da067623e9e4462b8d07eaea12df4b5f76c9b1ef
Ubuntu <ubuntu@ip-172-31-14-76.us-west-1.compute.internal>
2017-03-06 00:13:04 +0000

    Adding updated model and ipython play book for trying out stuff.

--- model.py
+++ model.py
@@ -32,25 +32,32 @@ def buildModel(feature_extract = False, ipshape = (160, 320, 3)):
     model = Sequential()
     ### Data Pre-processing
     #Cropping model
-    #model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=ipshape))
     #Normalization and mean shifting 
-    #model.add(Lambda(lambda x: x / 255.0 - 0.5))
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=ipshape))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5))
+    #model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=ipshape))
 
     #Convolutional Layers
     model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
-    #model.add(Dropout(0.25))
+    model.add(Dropout(0.2))
     model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
+    model.add(Dropout(0.25))
     model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
+    model.add(Dropout(0.3))
     model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Dropout(0.35))
     model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Dropout(0.4))
 
     model.add(Flatten())
     
     #Fully Connected Layers
     model.add(Dense(100))
+    model.add(Activation("relu"))
     model.add(Dense(50))
+    model.add(Activation("relu"))
     model.add(Dense(10))
+    model.add(Activation("relu"))
 
     if not feature_extract:
         model.add(Dense(1))
@@ -72,7 +79,12 @@ def getInputSamples():
             if skipHeader:
                 skipHeader = False
                 continue
-            samples.append((line[0], line[3]))
+            #image file, steering input, is image flipped
+            samples.append((line[0], float(line[3]), False))
+            #append a flipped image/steering input too to account for left turn bias
+            #The actual flipping happens in dataGenerator
+            samples.append((line[0], float(line[3]) * -1.0, True))
+    
     return samples
 
 
@@ -87,7 +99,13 @@ def dataGenerator(samples, batchsz = 32):
                 ifile = DATADIR + b[0]
                 img = cv2.imread(ifile)
                 steering = float(b[1])
-                X.append(img)
+                #If this was an image intended to be
+                #flipped, then do flip. The steering
+                #input is already flipped in getInputSamples.
+                if b[2] == True:
+                    X.append(np.fliplr(img))
+                else:
+                    X.append(img)
                 y.append(steering)
             X = np.array(X)
             y = np.array(y)
@@ -96,14 +114,14 @@ def dataGenerator(samples, batchsz = 32):
 
 def trainModel(model, samples, valsplit = 0.2, modeloutfile = "./model/model.h5"):
     train_samples, val_samples = train_test_split(samples, test_size = valsplit)
-    trainDataGen = dataGenerator(train_samples, batchsz = 32)
-    valDataGen = dataGenerator(val_samples, batchsz = 32)
-    model.compile(loss = "mse", optimizer="adam", metrics=["accuracy"])
+    trainDataGen = dataGenerator(train_samples, batchsz = 128)
+    valDataGen = dataGenerator(val_samples, batchsz = 128)
+    model.compile(loss = "mse", optimizer="adam", metrics=["mean_squared_error"])
     history = model.fit_generator(trainDataGen,\
                                   samples_per_epoch = len(train_samples),\
                                   validation_data = valDataGen,\
                                   nb_val_samples = len(val_samples),\
-                                  nb_epoch = 10)
+                                  nb_epoch = 30)
     model.save(modeloutfile)
     print("Model saved to \"", modeloutfile, "\"")
     return history
